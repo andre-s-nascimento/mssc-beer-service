fd97f8ce092c8903c77e8af72c383ac9c11d0425
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-10-20 16:37:56 -0300

    Criado Models, adicionado JPA para poder usar o PageImpl

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEmum.java
@@ -0,0 +1,14 @@
+package guru.springframework.msscbeerservice.web.model;
+
+public enum BeerStyleEmum {
+  LAGER,
+  PILSNER,
+  STOUT,
+  GOSE,
+  PORTER,
+  ALE,
+  WHEAT,
+  IPA,
+  PALE_ALE,
+  SAISON
+}
