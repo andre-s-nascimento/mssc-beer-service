470c152d4cdb5968089f3182ccea1d6331c3e9ad
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-11-10 15:03:35 -0300

    Após aula 169 - HikariCP config

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -14,10 +14,6 @@ public class BeerLoader implements CommandLineRunner {
   public static final String BEER_2_UPC = "0631234300019";
   public static final String BEER_3_UPC = "0083783375213";
 
-  public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
-  public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
-  public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
-
   private final BeerRepository beerRepository;
 
   public BeerLoader(BeerRepository beerRepository) {
@@ -26,7 +22,7 @@ public class BeerLoader implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    //loadBeerObjects();
+    loadBeerObjects();
   }
 
   private void loadBeerObjects() {
@@ -38,7 +34,7 @@ public class BeerLoader implements CommandLineRunner {
               .quantityToBrew(200)
               .minOnHand(12)
               .upc(BEER_1_UPC)
-              .price(new BigDecimal("12.95"))
+              .price(new BigDecimal("10.95"))
               .build());
 
       beerRepository.save(
@@ -58,7 +54,7 @@ public class BeerLoader implements CommandLineRunner {
               .quantityToBrew(200)
               .minOnHand(12)
               .upc(BEER_3_UPC)
-              .price(new BigDecimal("11.95"))
+              .price(new BigDecimal("12.95"))
               .build());
     }
   }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+--CREATE TABLE IF NOT EXISTS BEER (id VARCHAR(255) PRIMARY KEY, beer_name VARCHAR(255), beer_style VARCHAR(255), created_date TIMESTAMP, last_modified_date TIMESTAMP, min_on_hand INT, quantity_to_brew INT, price NUMERIC(20, 2), upc VARCHAR(255), version BIGINT);
+
+INSERT INTO beer (id, beer_name, beer_style, created_date, last_modified_date, min_on_hand, quantity_to_brew, price, upc, version ) values ('0a818933-087d-47f2-ad83-2f986ed087eb', 'Mango Bobs', 'IPA', CURRENT_TIMESTAMP , CURRENT_TIMESTAMP , 12,  200, 10.95, '0631234200036', 1);
+INSERT INTO beer (id, beer_name, beer_style, created_date, last_modified_date, min_on_hand, quantity_to_brew, price, upc, version ) values ('a712d914-61ea-4623-8bd0-32c0f6545bfd', 'Galaxy Cat', 'PALE_ALE', CURRENT_TIMESTAMP , CURRENT_TIMESTAMP , 12,  200, 11.95, '0631234300019', 1);
+INSERT INTO beer (id, beer_name, beer_style, created_date, last_modified_date, min_on_hand, quantity_to_brew, price, upc, version ) values ('026cc3c8-3a0c-4083-a05b-e908048c1b08', 'Pinball Porter', 'PORTER', CURRENT_TIMESTAMP , CURRENT_TIMESTAMP , 12,  200, 12.95, '0083783375213', 1);
